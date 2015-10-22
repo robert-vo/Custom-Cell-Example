cf58bcda0c5f052ff1f0c537b7498ef94e384d4c
robert-vo <robertvo79@gmail.com>
2015-10-21 22:41:34 -0500

    New Game object with attributes title, ratings, and description.

---
+++ CustomCellExample/Game.h
@@ -0,0 +1,17 @@
+//
+//  Game.h
+//  CustomCellExample
+//
+//  Created by Robert Vo on 10/21/15.
+//  Copyright © 2015 Robert Vo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Game : NSObject
+
+@property NSString *gameTitle;
+@property NSString *gameRating;
+@property NSString *gameDescription;
+
+@end
---
+++ CustomCellExample/Game.m
@@ -0,0 +1,13 @@
+//
+//  Game.m
+//  CustomCellExample
+//
+//  Created by Robert Vo on 10/21/15.
+//  Copyright © 2015 Robert Vo. All rights reserved.
+//
+
+#import "Game.h"
+
+@implementation Game
+
+@end
