437fcfa1b11979ea9c344a262caaa3a65e442ffa
robert-vo <robertvo79@gmail.com>
2015-10-21 22:23:37 -0500

    New File.

---
+++ CustomCellExample/MyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TheTableViewController.h
+//  CustomCellExample
+//
+//  Created by Robert Vo on 10/21/15.
+//  Copyright © 2015 Robert Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+
+@end
